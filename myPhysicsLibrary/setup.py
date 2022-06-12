a7d211ab4b7de3cf48ffd05bda6ed14cd5c5207a
Logan Yamamoto <lkyamamo@usc.edu>
2022-06-11 17:21:53 -1000

    test library

---
+++ myPhysicsLibrary/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'mypythonlib',
+    packages = find_packages(),
+    version='0.1.0',
+    description='My first Python Library',
+    author = 'Me',
+    license = 'Also Me',
+)
